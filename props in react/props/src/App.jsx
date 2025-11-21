4d9fe2b09f213534e6b8431499ab130fb68790ca
Satish <dadassatish6@gmail.com>
2025-11-21 22:11:17 +0530

    new

---
+++ props in react/props/src/App.jsx
@@ -0,0 +1,12 @@
+import { Component, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <Student name="Satya" age={21} />
+  );
+}
+
+
---
+++ props in react/props/src/student.jsx
@@ -0,0 +1,9 @@
+function Student(props) {
+  return (
+    <h1>
+      My name is {props.name} and I am {props.age} years old.
+    </h1>
+  );
+}
+
+export default Student;
