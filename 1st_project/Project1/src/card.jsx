21699e1b1d725d38a16073af06851d112f67e101
Satish <dadassatish6@gmail.com>
2025-12-13 00:50:16 +0530

    nw

--- 1st_project/Project1/src/card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card(){
-    return(
-        <div style={{border:"2px solid red" , height:"9rem" , width:"9rem", marginTop:"15rem"}}>
-            <h1>hello</h1>
-        </div>
-    )
-}
